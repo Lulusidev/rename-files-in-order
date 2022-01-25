50f47f26128b9beb6a58f6ae246255325b17a3a8
lulusidev <TLulusi@protonmail.com>
2022-01-25 16:02:56 -0300

    v.1

---
+++ rename_order.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+#lol
+
+#Error or Help
+if [ $# -ne 2 ] && [ $# -ne 1 ] 
+then
+    echo "Usage: rename.sh [PATH] [first_number] "
+    exit 1 
+#default begin number
+elif[ $# -e 1 ]
+
+    begin_number=0
+    path="$1"
+
+#2 args
+else
+    path="$1"
+    begin_number=$2
+fi
+
+main(){
+    #initilizing counter for use in name
+    j=$begin_number
+    
+    for file in $path/*;do
+            
+        file_extension=${file##*.}
+        #format string of filename
+        new_name=$path/$(printf "%03i.%s" $j "$file_extension")
+
+        mv $file $new_name
+        #add 1 for counter    
+        let j=$j+1
+
+    done
+}
+
+main
